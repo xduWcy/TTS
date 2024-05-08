deb08d08898144c99ff498f7dd001bbffff644c3
xduWcy <w1539652912@163.com>
2024-05-08 16:33:00 +0800

    ui_design
    TXT阅读器-UI设计

---
+++ ui_designer/UIReader.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from ui_designer.untitled import Ui_MainWindow
+
+
+class MyMainWindow(QMainWindow, Ui_MainWindow): # 继承 QMainWindow 类和 Ui_MainWindow 界面类
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)  # 初始化父类
+        self.setupUi(self)  # 继承 Ui_MainWindow 界面类
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)  # 在 QApplication 方法中使用，创建应用程序对象
+    myWin = MyMainWindow()  # 实例化 MyMainWindow 类，创建主窗口
+    myWin.show()  # 在桌面显示控件 myWin
+    sys.exit(app.exec_())  # 结束进程，退出程序
